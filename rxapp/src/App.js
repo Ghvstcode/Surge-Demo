17e07b768469e18e35f84c21aecc39f9057b03f7
Ghvstcode <impoissants@gmail.com>
2021-01-20 12:27:23 +0100

    githooks

--- rxapp/src/App.js
+++ rxapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Autodeployoing React Applications to Surge with githooks</p>
+        <p>Autodeploying React/Vue Applications to Surge with githooks</p>
         <a
           className="App-link"
           href="https://reactjs.org"
